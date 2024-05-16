73f767b99e38c7b46366cac7d8411a945ff1790f
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2024-05-16 23:28:29 +0300

    Update welcome-form-input.jsx

--- src/components/welcome-form/welcome-form-input.jsx
+++ src/components/welcome-form/welcome-form-input.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "../../pages/Welcome"
 import "./welcome-form-input.css"
 
-export default function WelcomeFormInput({name, placeholder}) {
+export default function WelcomeFormInput({name, placeholder, onChange}) {
     return (
         <div className="find__infoblolck">
             <div className="find__name">
                 {name}
             </div>
-            <input type="text" placeholder={placeholder} className="find__input"></input>
+            <input type="text" placeholder={placeholder} className="find__input" onChange={onChange} ></input>
         </div>
     );
   }
