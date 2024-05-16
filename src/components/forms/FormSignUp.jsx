086c4f35b309af33e8494f4a1c0d8ebfa3295500
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2024-05-16 23:23:02 +0300

    Update FormSignUp.jsx

--- src/components/forms/FormSignUp.jsx
+++ src/components/forms/FormSignUp.jsx
@@ -1,55 +1,50 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Eye from "../../img/eye.png";
-import "./FormSignUp.css";
+import "./FormLogin.css";
 import axios from "axios";
-import { useAuth } from "../../session/AuthContext";
+import { useAuth } from '../../session/AuthContext';
 
-function FormSignUp() {
-  const [name, setName] = useState("");
+function FormLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [rememberMe, setRememberMe] = useState(false);
+  const [error, setError] = useState("")
+  const { login } = useAuth();
+  const navigate = useNavigate();
   const togglePassword = () => {
     setShowPassword(!showPassword);
   };
-  const { login } = useAuth();
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:3001/register', { name, email, password })
-    .then(response => {
-      setEmail("");
-      setPassword("");
-      setName("");
-      setError("");
-      login(response.data)
-      navigate('/welcome');
-    }).catch(error => {
+    axios
+      .post(`http://localhost:3001/login`, { email, password })
+      .then((response) => {
+        setEmail("");
+        setPassword("");
+        setError("");
+        login(response.data);
+        navigate("/welcome");
+      })
+      .catch((error) => {
         setError(error.response.data.message);
-    });
+      });
+  };
+
+  const rememberUser = (e) => {
+    // TODO: Implement remember user functionality
   };
 
   return (
     <div className="middle-item">
       <div className="registration-top">
-        <h2>Create Account</h2>
+        <h2>Welcome Back</h2>
         <h5>Enter your credentials to access your account.</h5>
       </div>
       <form className="form-login" onSubmit={handleSubmit}>
-        <div className="form-row">
-          <label htmlFor="name">Name:</label>
-          <input
-            className="form-row-input"
-            type="text"
-            value={name}
-            placeholder="Enter your name"
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
         <div className="form-row">
           <label htmlFor="email">Email:</label>
           <input
@@ -79,7 +74,7 @@ function FormSignUp() {
               checked={showPassword}
               onChange={togglePassword}
               hidden
-            ></input>
+            />
             <label htmlFor="show-password">
               <img src={Eye} alt="" />
             </label>
@@ -87,22 +82,27 @@ function FormSignUp() {
         </div>
         {error && <p className="error">{error}</p>}
         <div className="form-row terms-and-conditions">
-          <input type="checkbox" id="agree" required />
-          <label htmlFor="agree">
-            Agree with company{" "}
-            <a href="/terms-and-conditions">terms and conditions.</a>
-          </label>
+          <input
+            type="checkbox"
+            id="agree"
+            checked={rememberMe}
+            onChange={(e) => setRememberMe(e.target.checked)}
+          />
+          <label htmlFor="agree">Remember me</label>
         </div>
         <button className="sign-up-button" type="submit">
-          Sign Up
+          Log In
         </button>
       </form>
-      <div className="registration-bottom">
-        <h2>Already have an Account?</h2>
-        <a href="/login">Log In</a>
+      <div className="registration-pre-bottom">
+        <a href="/forgot-password">Forgot Password?</a>
+        <div className="registration-bottom">
+          <h2>Don't have an account?</h2>
+          <a href="/signup">Create Account</a>
+        </div>
       </div>
     </div>
   );
 }
 
-export default FormSignUp;
+export default FormLogin;
