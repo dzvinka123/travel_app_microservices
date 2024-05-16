b2330283fa90ebe123a6f9c149817754e9dff4fa
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2024-05-16 13:38:03 +0300

    Update CreateTripNewUser.jsx

--- src/pages/CreateTripNewUser.jsx
+++ src/pages/CreateTripNewUser.jsx
@@ -1,3 +1,4 @@
+
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import Driver from "../components/newuser/Driver";
@@ -12,7 +13,7 @@ import booking from "../img/booking.png"
 import "../components/newuser/createdTripUser.css";
 
 
-export default function CreateTripNewUser() {
+export default function CreateTripNewUser() { 
     const city = 'Lviv';
     const days = "18.05.2024 - 23.05.2024";
     
@@ -22,20 +23,32 @@ export default function CreateTripNewUser() {
         {/* weather block */}
         <Weather days_range={days} city={city} />
         {/* 2 block */}
-        <div class="ride-text">
+        <div className="ride-text">
             <h2>Secure Your Ride: Booking Transport</h2>
         </div>
             
+        <div className="driver-block">
+            <h3>Available cars for ride</h3>
+            <span>Fri, Apr 26 2024</span> {/* this span will be dynamic */}
+            <div className="drivers-container">
+                <div className="driver-col">
+                    {/* change css to make it cols with 2 elements */}
+                    <Driver person={person} plus={plus} />
+                    <Driver person={person} plus={plus}/>
+                </div>
+            </div>
+        </div>
+            
         {/* 3 block */}
-        <div class="hotel-text">
+        <div className="hotel-text">
             <h2>Find Your Home Away From Home: Booking Accommodation</h2>
         </div>
             
-        <div class="hotel-block">
+        <div className="hotel-block">
             <h3>Browse Accommodation Options: Available Hotels</h3>
             <span>Fri, Apr 26 2024 - Mon, Apr 29 2024</span> {/* this span will be dynamic */}
-            <div class="hotel-cards">
-                <div class="hotel-col">
+            <div className="hotel-cards">
+                <div className="hotel-col">
                     {/* change css to make it cols with 2 elements */}
                     <HotelCard plus={plus} booking={booking}/>
                     <HotelCard plus={plus} booking={booking}/>
@@ -44,23 +57,23 @@ export default function CreateTripNewUser() {
         </div>
     
         {/* 5 block */}
-        <section class="additional-container">
-            <div class="todo-list">
-                <div class="todo-text">
+        <section className="additional-container">
+            <div className="todo-list">
+                <div className="todo-text">
                     <h2>Additional Information</h2>
                 </div>  
                     
-                <div class="todo-box">
-                    <div class="todo-box-text">
+                <div className="todo-box">
+                    <div className="todo-box-text">
                         <h4>To-Do List</h4> 
                     </div>
-                    <div class="checkboxes">
-                        <div class="checkbox-list">
-                            <ToDos text={"Research local cafes for breakfast spots in Yaremche"}/>
-                            <ToDos text={"Book tickets for the Waterfall Probiy"} />
-                            <div class="todo-button">
+                    <div className="checkboxes">
+                        <div className="checkbox-list">
+                            <ToDos text={"Research local cafes for breakfast spots in Yaremche"} id={"box1"} />
+                            <ToDos text={"Book tickets for the Waterfall Probiy"} id={"box2"} />
+                            <div className="todo-button">
                                 <AddButton plus={plus} />
-                                <label for="box2"><input placeholder="Add your tasks here..."/></label><br/> {/* this might be dynamic */}
+                                <label htmlFor="box2"><input placeholder="Add your tasks here..."/></label><br/> {/* this might be dynamic */}
                             </div>
                         </div> 
                     </div>
@@ -72,4 +85,4 @@ export default function CreateTripNewUser() {
         <Footer />
       </>
     );
-  }
+}
