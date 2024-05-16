4892ca9f121294ec9eedc9e2074e0522c8a86c84
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2024-05-16 23:21:45 +0300

    Update Form-Input.jsx

--- src/components/formInput/Form-Input.jsx
+++ src/components/formInput/Form-Input.jsx
@@ -2,13 +2,15 @@ import "./Form-Input.css"
 import { useRef } from 'react';
 
 
-export default function FormInput({ icon, placeholder, text, name, className }) {
-
+export default function FormInput({ icon, placeholder, text, name, onChange, onClick, value, className }) {
     const inputRef = useRef(null);
 
     const handleDivClick = () => {
         if (inputRef.current) {
             inputRef.current.focus();
+        }
+        if (onClick) {
+            onClick();
         }
     };
 
@@ -17,7 +19,15 @@ export default function FormInput({ icon, placeholder, text, name, className }) 
             <img className="search-icon" src={icon} alt={`${text} Icon`} />
             <div className="search-field">
                 <label htmlFor={name}>{text}</label>
-                <input ref={inputRef} type="text" id={name} name={name} placeholder={placeholder} required />
+                <input ref={inputRef} 
+                    type="text" 
+                    id={name} 
+                    name={name} 
+                    placeholder={placeholder} 
+                    onChange={onChange}
+                    value={value}
+                    required 
+                />
             </div>
         </div>
     );
