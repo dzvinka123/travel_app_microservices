486a4ce0458c0314710d7b8415a90599a656d51d
zahar-kohut-ucu <116507761+zahar-kohut-ucu@users.noreply.github.com>
2024-05-17 13:25:49 +0300

    Update Journey-Widget.jsx

--- src/components/journeys/Journey-Widget.jsx
+++ src/components/journeys/Journey-Widget.jsx
@@ -1,21 +1,31 @@
 import "./widgetstyles.css"
 import Map from "./Map"
 import ToDoList from './ToDo-List';
-import VisitPlace from "../places-to-visit/VisitPlace"
 import Weather from "../newuser/Weather"
+import VisitPlace from "../places-to-visit/VisitPlace"
 import left from "../../img/left.svg"
 import right from "../../img/rigth.svg"
-import { Wrapper } from "@googlemaps/react-wrapper";
 import "./Places.css"
+import { Wrapper } from "@googlemaps/react-wrapper";
+import { useState, useEffect, useContext } from "react";
+import Friend from "../friends/Friend";
+import "../friends/Friends.css";
+import { AuthContext } from "../../session/AuthContext";
 
-export default function JourneyWidget({ onClose, id, destination, description, startDate, endDate, toDos, emails }) {
+
+export default function JourneyWidget({ handleJourneyUpdate, onClose, id, destination, description, startDate, endDate, toDos, emails }) {
+    const { user } = useContext(AuthContext)
     const date = `${startDate} - ${endDate}`
     const [todos, setToDos] = useState(toDos)
     const handleToDoUpdate = (newToDoList) => {
         setToDos(newToDoList);
     };
+    useEffect(() => {
+        handleJourneyUpdate(todos);
+        //console.log(cards)
+      }, [todos, handleJourneyUpdate])
     return (
-        <Wrapper apiKey={import.meta.env.VITE_REACT_APP_GOOGLE_API} libraries={['marker']}>
+        <Wrapper apiKey={import.meta.env.VITE_REACT_APP_GOOGLE_API} libraries={['marker', 'places']}>
             <div className="modal-container">
                 <div className="modal-content">
                     <span className="close" onClick={onClose}>&times;</span>
@@ -24,6 +34,11 @@ export default function JourneyWidget({ onClose, id, destination, description, s
                             <div className="block first-block">
                                 <p className="widget-header">Journey To <br /> {destination}</p>
                                 <p className="widget-description">{description}</p>
+                                <div className="friends__friends">
+                                    {emails.filter(email => email.user_email !== user.email).map((email, index) => (
+                                        <Friend key={index} email={email.user_email} onDelete={() => {}}/>
+                                        ))}
+                                </div>
                                 {/* Add friends functionality */}
                             </div>
                             <Weather days_range={date} city={destination} />
