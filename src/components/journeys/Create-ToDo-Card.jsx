869a1a7b2b6891a0a32904f38a4d9c6daff68fb5
zahar-kohut-ucu <116507761+zahar-kohut-ucu@users.noreply.github.com>
2024-05-17 13:19:54 +0300

    Update Create-ToDo-Card.jsx

--- src/components/journeys/Create-ToDo-Card.jsx
+++ src/components/journeys/Create-ToDo-Card.jsx
@@ -1,17 +1,34 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import add from '../../img/add.svg';
 import "./widgetstyles.css";
+import axios from 'axios';
 
-export default function CreateCard({ addNewCard }) {
+export default function CreateCard({ card_id, addNewCard }) {
     const [inputValue, setInputValue] = useState('');
-
+    //const [newCard, setNewCard] = useState();
     const handleAddCard = () => {
         if (inputValue.trim() !== '') {
-            addNewCard(inputValue.trim());
-            setInputValue('');
+            axios.post(`http://localhost:3001/todo-list`, {task: inputValue.trim(), done: 0, card_id: card_id})
+                .then(response => {
+                    const { success, message, taskId } = response.data;
+                    if (success) {
+                        const newCard = { id: taskId, task: inputValue.trim(), done: 0, card_id: card_id };
+                        addNewCard(newCard);
+                        setInputValue('');   
+                    } else {
+                        console.error("Failed to fetch journeys:", response.data.message);
+                    }
+                })
+                .catch(error => {
+                    console.error("Failed to fetch journeys:", error);
+                });
         }
     };
+    // useEffect(() => {
+    //     addNewCard(newCard);
+    //   }, [newCard, addNewCard])
+
 
     return (
         <div className="cards-container">
