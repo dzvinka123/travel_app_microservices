f0ac31cf42fda11a3950a5c92218a98d5e118339
dzvinka123 <116108850+dzvinka123@users.noreply.github.com>
2024-05-17 04:30:30 +0300

    Update Map.jsx

--- src/components/journeys/Map.jsx
+++ src/components/journeys/Map.jsx
@@ -82,6 +82,6 @@ export default function Map({ destination }) {
   }, [map, coords]);
 
   return (
-    <div ref={ref} id="map" style={{ width: "100%", height: "400px" }}></div>
+    <div ref={ref} id="map" style={{ width: "100%", height: "130px" }}></div>
   );
 }
