a4ea690ae991ba87b9c66689837f5184f19f1e6a
zahar-kohut-ucu <116507761+zahar-kohut-ucu@users.noreply.github.com>
2024-05-17 13:21:02 +0300

    Update Journey-Option.jsx

--- src/components/journeys/Journey-Option.jsx
+++ src/components/journeys/Journey-Option.jsx
@@ -8,10 +8,13 @@ export default function JourneyOption({ onButtonClick, to, from, startDate, endD
         <div className="journeys-list-item">
             <img className="journeys-item-image" src={placeholder} alt="Placeholder Image for a journey" />
             <span className="journey-item-country">{to}</span>
+            {/* <span className="journey-item-country">{country}</span> */}
             <div className="journey-preview-wrapper">
                 <div className="journey-info">
                     <span className="journey-item-destination">Journey from {from} to {to}</span>
+                    {/* <span className="journey-item-destination">{title}</span> */}
                     <span className="journey-item-date">{startDate} - {endDate}</span>
+                    {/* <span className="journey-item-date">{date}</span> */}
                 </div>
                 <button className="journeys-button" onClick={onButtonClick}>
                     <img className="journeys-item-preview" src={openPreview} alt="Preview Open Icon" />
