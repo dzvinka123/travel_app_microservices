b328650d312d7c9c6bcf8be514fd68bb53eb1c55
zahar-kohut-ucu <116507761+zahar-kohut-ucu@users.noreply.github.com>
2024-05-17 14:20:31 +0300

    Update ToDo-Card.jsx

--- src/components/journeys/ToDo-Card.jsx
+++ src/components/journeys/ToDo-Card.jsx
@@ -26,14 +26,14 @@ export default function ToDoCard({ handleOnCheck, checkboxIndex, input }) {
     return (
         <div className="card-wrapper">
             <div className="checkbox-wrapper">
-                <input className="inp-cbx" id={`cbx-${checkboxIndex}`} type="checkbox" checked={input.done} onChange={onClickUpdate}/>
+                <input className="inp-cbx" id={`cbx-${checkboxIndex}`} type="checkbox" checked={input.done ? input.done : 0} onChange={onClickUpdate}/>
                 <label className="cbx" htmlFor={`cbx-${checkboxIndex}`}>
                     <span>
                         <svg width="12px" height="10px" viewBox="0 0 12 10">
                             <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
                         </svg>
                     </span>
-                    <span>{input.task}</span>
+                    <span>{input.task ? input.task : input}</span>
                 </label>
             </div>
         </div>
