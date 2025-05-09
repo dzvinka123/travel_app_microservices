3a1571a6576ccd862c0b20d0eef56f925218718b
khrystynamk <khrystyna.mysak@gmail.com>
2025-05-09 18:25:10 +0300

    fix: removed redundant function

--- src/components/places-to-visit/VisitPlace.jsx
+++ src/components/places-to-visit/VisitPlace.jsx
@@ -35,7 +35,7 @@ export default function VisitPlace({ city }) {
   useEffect(() => {
     const loadCity = city || to;
     if (loadCity) {
-      loadPlaces(loadCity).then(setPlaces).catch(console.error);
+      loadPlaces(loadCity).then(setPlaces).catch(console.error); // change here to request to GppglePlacesService
     }
   }, [city, to]);
 
@@ -77,58 +77,3 @@ export default function VisitPlace({ city }) {
     </div>
   );
 }
-
-const loadPlaces = async (city) => {
-  const service = new google.maps.places.PlacesService(document.createElement('div'));
-  const coords = fetchCoords(city);
-  const request = {
-    location: { lat: (await coords).latitude, lng: (await coords).longitude },
-    radius: "500",
-    type: ["tourist_attraction"],
-  };
-
-  return new Promise((resolve, reject) => {
-    service.nearbySearch(request, async (results, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        const placesDetails = await Promise.all(results.map(async place => {
-          const detail = await getPlaceDetails(service, place.place_id);
-          const photoUrl = place.photos && place.photos.length > 0 ? place.photos[0].getUrl() : 'placeholder-image-url';
-          const rating = place.rating ? `Rating: ${place.rating} (${place.user_ratings_total} reviews)` : "Rating not available";
-
-          return {
-            id: place.place_id,
-            name: place.name,
-            address: place.vicinity,
-            rating: rating,
-            hours: detail.hours,
-            imageUrl: photoUrl
-          };
-        }));
-        resolve(placesDetails);
-      } else {
-        reject(status);
-      }
-    });
-  });
-};
-
-const getPlaceDetails = async (service, placeId) => {
-  const request = {
-    placeId: placeId,
-    fields: ['rating', 'user_ratings_total', 'opening_hours']
-  };
-
-  return new Promise(resolve => {
-    service.getDetails(request, (result, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        let hours = "Work hours not available";
-        if (result.opening_hours) {
-          hours = result.opening_hours['weekday_text'][new Date().getDay() - 1]
-        }
-        resolve({ hours });
-      } else {
-        resolve({ hours: "Work hours not available" });
-      }
-    });
-  });
-};
